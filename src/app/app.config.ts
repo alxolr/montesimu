64aa5b1669b3c93b49b406b71a1de3d446fc9f22
Alexandru Olaru <alxolr@gmail.com>
2025-11-17 14:53:52 +0200

    Added primeng and tailwindcss for the ui part

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,25 @@ import {
   provideZoneChangeDetection,
 } from "@angular/core";
 import { provideRouter } from "@angular/router";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
 
 import { routes } from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: false // This forces light mode
+        }
+      },
+    })
   ],
 };
